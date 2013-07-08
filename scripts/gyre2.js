8c13feae8e6a1a174688635a16930a652998ef5d
iomz <iomz@sfc.wide.ad.jp>
2013-07-09 01:26:47 +0900

    Completion without animation

--- scripts/gyre2.js
+++ scripts/gyre2.js
@@ -14,15 +14,13 @@ function drawOneChar(ctx, c, angle, len, radius) {
 }
 
 function drawTextAlongArc(ctx, str, centerX, centerY, radius, angle) {
-    var len = str.length, c;
+    var len = str.length;
     ctx.save();
     ctx.translate(centerX, centerY);
     ctx.rotate(-1 * angle / 2);
     ctx.rotate(-1 * (angle / len) / 2);
     for(var n = 0; n < len; n++) {
-        c = str[n];
-        //window.setTimeout(function(){drawOneChar(ctx, c, angle, len, radius)},1000);
-        drawOneChar(ctx, c, angle, len, radius);
+        drawOneChar(ctx, str[n], angle, len, radius);
     }
 }
 
@@ -36,18 +34,31 @@ function getString(file) {
     return xmlhttp.responseText;
 }
 
-var genre = ['regeneration','meme'][Math.floor(Math.random()*2)];
+var list = ['regeneration','meme','nuclear'];
+var genre = list[Math.floor(Math.random()*list.length)];
 var str = getString('data/'+genre);
 var strlen = str.length;
 var canvas = document.getElementById('canv');
-var allign = 200;
 ctx = canvas.getContext('2d');
 ctx.fillStyle = 'white';
 centerX = canvas.width / 2;
 centerY = canvas.height / 2;
-angle = Math.PI * (Math.random() + 1.5);
 
-document.getElementById('pi').innerHTML="Genre: " + genre + ", &pi;: " + angle.toString();
+// Angle of the arc
+var angle = Math.PI * (Math.random() + 1);
+
+// Number of characters on one arc
+var allign = 200;
+
+// Interval between the arcs
+var interval = Math.floor(Math.random()*5)+1;
+
+var outerRad = Math.floor(Math.random()*200)+400;
+
+// Display animation info 
+document.getElementById('info').innerHTML="Genre: " + genre + ", Interval: " + interval.toString() + ", Outer radius: " + outerRad.toString() + ", &pi;: " + angle.toString();
+
+// Audio setup
 var audio = document.getElementById('audio');
 var ogg = document.createElement('source');
 ogg.setAttribute('src',"audio/"+genre+".ogg");
@@ -58,9 +69,8 @@ mp3.setAttribute('src',"audio/"+genre+".mp3");
 mp3.setAttribute('type',"audio/mp3");
 audio.appendChild(mp3);
 
-for(var radius=500,i=0; 12<radius; radius-=4,i++) {
+for(var radius=outerRad,i=0; 13<radius; radius-=interval,i++) {
     ctx.font = (13-i*.1).toString() + 'pt Georgia';
     txt = str.substring((i%(strlen/allign))*allign, (i%(strlen/allign))*allign+allign); 
     drawTextAlongArc(ctx, txt, centerX, centerY, radius, angle);
 }
-
